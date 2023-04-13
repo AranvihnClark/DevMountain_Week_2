e3159429e19a49428634bdf58eeee90c18f44c0c
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-04-13 11:39:44 -0400

    Added regex and methods version for palindrome checker

--- Thursday/challenge-is-palindrome/isPalindrome.js
+++ Thursday/challenge-is-palindrome/isPalindrome.js
@@ -1,18 +1,49 @@
 // Write your code below
 function palindromeCheck(word) {
     for(let i = 0; i < word.length; i++) {
-        for(let j = (word.length - 1) - i; j >= i; j++) {
-            if(i === j && word[i] === word[j]) break;
-            else if(word[i] === word[j]) break;
-            else return console.log(`"${word}" is not a palindrome.`);
-        }
+        let j = word.length - 1 - i;
+        if(i === j && word[i] === word[j]) break;
+        else if(word[i] === word[j]) break;
+        else return console.log(`"${word}" is not a palindrome.`);
+        
     }
-    return console.log(`"${word}" is a true palindrome.`)
+    return console.log(`"${word}" is a true palindrome.`);
 }
+console.log(`Checking via two for loops.\n`);
 
 palindromeCheck(`rotator`);
 palindromeCheck(`Noon`);
+palindromeCheck(`Hannah`);
 palindromeCheck(`peep`);
-palindromeCheck(`yo banana boy`);
-palindromeCheck(`Kayak`);
-palindromeCheck(`step on no pets`);
+
+console.log(`\nNow to check via regex pattern.\n`);
+
+function palindromeVerify(str)
+{
+  let evalStr = str.replace(/[^A-Za-z0-9]/g, ' ');
+  for (let i = 0; i < evalStr.length/2; i++)
+  {
+    if(evalStr[i] !== evalStr[evalStr.length - 1 - i])
+    {
+      return console.log(`"${str}" is not a palindrome.`);
+    }
+  }
+  return console.log(`"${str}" is a true palindrome.`);
+}
+
+palindromeVerify(`Kayak`);
+palindromeVerify(`Radar`);
+palindromeVerify(`deified`);
+palindromeVerify(`detartrated`);
+
+
+console.log(`\nAnd, lastly, we check via methods.\n`);
+function palindromeAgain(phrase) {
+    if(phrase.split('').reverse().join(``) === phrase) return console.log(`"${phrase}" is a true palindrome.`);
+    else return console.log(`"${phrase}" is not a palindrome.`);
+}
+
+palindromeAgain(`step on no pets`);
+palindromeAgain(`yo banana boy`);
+palindromeAgain(`noel sees leon`);
+palindromeAgain(`he lived as a devil eh`);
